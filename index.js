7041fcc3e256bb3f909ead0c3f73e1124be051ac
Luis Armando <cruz.luisarmando@gmail.com>
2024-12-21 08:51:47 -0600

    color y mensaje

--- index.js
+++ index.js
@@ -8,6 +8,12 @@ function SaludosFunc(value, index, array) {
   p.innerHTML = value;
   document.body.appendChild(p);
 }
+
+m = document.createElement("p");
+m.id = "pm";
+m.innerHTML = "我想要食物、房子和汽车。\n你能帮助我吗？";
+document.body.appendChild(m);
+
 var link = document.createElement("a");
 link.href = "https://ko-fi.com/luis239732";
 link.innerHTML = "Donation";
