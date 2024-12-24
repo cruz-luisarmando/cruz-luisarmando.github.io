d055ccfd07eee15e8636a79e7300e595726b1467
Luis Armando <cruz.luisarmando@gmail.com>
2024-12-23 19:59:33 -0500

    emoji

--- index.js
+++ index.js
@@ -17,4 +17,28 @@ document.body.appendChild(m);
 var link = document.createElement("a");
 link.href = "https://ko-fi.com/luis239732";
 link.innerHTML = "Donation";
-document.body.appendChild(link);
+link.className =
+  "text-uppercase link-success link-opacity-50 link-opacity-50-hover link-offset-3 link-underline-opacity-10 link-underline-opacity-100-hover";
+
+var plink = document.createElement("p");
+plink.id = "plink";
+document.body.appendChild(plink);
+plink.appendChild(link);
+
+function emojiChange(value) {
+  if (value) {
+    console.log(value);
+    document.getElementById("ew").style.visibility = "hidden";
+    document.getElementById("eh").style.visibility = "visible";
+  } else {
+    document.getElementById("ew").style.visibility = "visible";
+    document.getElementById("eh").style.visibility = "hidden";
+  }
+}
+emojiChange(false);
+link.addEventListener("mouseover", function () {
+  emojiChange(true);
+});
+link.addEventListener("mouseleave", function () {
+  emojiChange(false);
+});
